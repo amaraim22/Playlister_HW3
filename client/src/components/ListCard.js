e0e1a1306141a18974ab94a7f2122335a0853728
Amara Im <amaraim@icloud.com>
2022-10-17 17:42:38 -0400

    editList fix

--- client/src/components/ListCard.js
+++ client/src/components/ListCard.js
@@ -11,9 +11,10 @@ import { GlobalStoreContext } from '../store'
 function ListCard(props) {
     const { store } = useContext(GlobalStoreContext);
     const [ editActive, setEditActive ] = useState(false);
-    const [ text, setText ] = useState("");
     store.history = useHistory();
     const { idNamePair, selected } = props;
+
+    const [ text, setText ] = useState(idNamePair.name);
 
     function handleLoadList(event) {
         if (!event.target.disabled) {
